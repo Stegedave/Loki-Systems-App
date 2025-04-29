82b4023a362f6a4083f45b546bcb969e0cf5213d
David Stegeman <stegedave@gmail.com>
2025-04-29 11:50:54 -0400

    Debugged database.py, had repeating functions and wrong database path

--- database.py
+++ database.py
@@ -34,8 +34,13 @@ def save_service_record(data):
     c.execute('''
         INSERT INTO services (date, time, service_provided, service_fee, service_notes)
         VALUES (?, ?, ?, ?, ?)
-    ''', (data["Date (YYYY-MM-DD)"], data["Time (HH:MM)"], data["Service Provided"],
-          data["Service Fee"], data["Service Notes"]))
+    ''', (
+        data["Date (YYYY-MM-DD)"],
+        data["Time (HH:MM)"],
+        data["Service Provided"],
+        data["Service Fee"],
+        data["Service Notes"]
+    ))
     conn.commit()
     conn.close()
 
@@ -56,38 +61,5 @@ def delete_service(service_id):
     conn.commit()
     conn.close()
 
-def save_service_record(data):
-    conn = sqlite3.connect('leethal.db')
-    c = conn.cursor()
-    c.execute('''
-        INSERT INTO services (date, time, service_provided, service_fee, service_notes)
-        VALUES (?, ?, ?, ?, ?)
-    ''', (
-        data["Date (YYYY-MM-DD)"],
-        data["Time (HH:MM)"],
-        data["Service Provided"],
-        data["Service Fee"],
-        data["Service Notes"]
-    ))
-    conn.commit()
-    conn.close()
-
-def fetch_all_services():
-    conn = sqlite3.connect('leethal.db')
-    c = conn.cursor()
-    c.execute('SELECT * FROM services')
-    
-    services = c.fetchall()
-    
-    conn.close()
-    return services
-
-
-def delete_service(service_id):
-    conn = sqlite3.connect('leethal.db')
-    c = conn.cursor()
-    c.execute('DELETE FROM services WHERE id = ?', (service_id,))
-    conn.commit()
-    conn.close()
 
 
